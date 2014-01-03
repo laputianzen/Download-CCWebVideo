b17511baa838d06e90758b0017169fbd6c499d8e
Peter Tsai <laputianzen@gmail.com>
2014-01-03 13:05:44 +0800

    Create M files and Video_List.txt, Keyframe.txt

---
+++ download_cc_web_video.m
@@ -0,0 +1,17 @@
+function download_cc_web_video()
+server_videoPath = 'http://vireo.cs.cityu.edu.hk/webvideo/videos/';
+storagePath = '/Volumes/SBPD/cc_web_video/videos/';
+
+[VideoID QueryID Source VideoName URL]=textread('Video_List.txt','%s%s%s%s%s','delimiter','\t');
+
+for i=4019:length(VideoID)
+    if (~exist([storagePath QueryID{i}],'dir'))
+        mkdir([storagePath QueryID{i}]);
+    end
+
+    i
+    VideoName{i}
+    urlwrite([server_videoPath QueryID{i} '/' VideoName{i}],[storagePath QueryID{i} '/' VideoName{i}]);
+end
+
+end
---
+++ download_cc_web_video_keyframes.m
@@ -0,0 +1,16 @@
+function download_cc_web_video_keyframes()
+server_videoPath = 'http://vireo.cs.cityu.edu.hk/webvideo/Keyframes/';
+storagePath = '/Volumes/SBPD/cc_web_video/Keyframes/';
+
+[SerialID KeyframeName VideoID VideoName]=textread('Shot_Info.txt','%s%s%s%s','delimiter','\t');
+
+for i=1:length(VideoID)
+    KID=VideoID{i}/100;
+    if (~exist([storagePath KID],'dir'))
+        mkdir([storagePath KID]);
+    end
+
+    urlwrite([server_videoPath KID '/' KeyframeName{i}],[storagePath KID '/' KeyframeName{i},'.jpg']);
+end
+
+end
