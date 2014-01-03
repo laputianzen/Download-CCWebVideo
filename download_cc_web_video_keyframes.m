221e5941f825fd683562fda9be2b1314a104cea6
Peter Tsai <laputianzen@gmail.com>
2014-01-03 13:35:27 +0800

    fix bug of calculating VideoID/100

--- download_cc_web_video_keyframes.m
+++ download_cc_web_video_keyframes.m
@@ -1,11 +1,13 @@
 function download_cc_web_video_keyframes()
+%  http://vireo.cs.cityu.edu.hk/webvideo/Keyframes/KID/KeyframeName       
+%  where KID = VideoID/100   
 server_videoPath = 'http://vireo.cs.cityu.edu.hk/webvideo/Keyframes/';
 storagePath = 'cc_web_video/Keyframes/';
 
 [SerialID KeyframeName VideoID VideoName]=textread('Shot_Info.txt','%s%s%s%s','delimiter','\t');
 
 for i=1:length(VideoID)
-    KID=VideoID{i}/100;
+    KID=int2str(floor(str2num(VideoID{i})/100));
     if (~exist([storagePath KID],'dir'))
         mkdir([storagePath KID]);
     end
